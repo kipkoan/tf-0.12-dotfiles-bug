3ee911d0488fe8658a4afba2183c825b79ad7b09
Kip McEntire <kipm@zillowgroup.com>
2019-09-27 18:00:10 +0000

    update sha pin

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,7 +7,7 @@ module "tf_dotfiles_1" {
 
   # local source using git sha: WORKS in tf 0.11.14
   # local source using git sha: DOES NOT WORK in tf 0.12.9 - local git source not supported
-  # source = "git::../..//modules/tf_dotfiles?ref=92d2d80821a"
+  # source = "git::../..//modules/tf_dotfiles?ref=a8bdfcc3a46"
 
   # remote source: WORKS in tf 0.11.14
   # remote source: DOES NOT WORK in tf 0.12.9 - 2nd module has no dotfile included
@@ -15,14 +15,14 @@ module "tf_dotfiles_1" {
 
   # remote source using git sha: WORKS in tf 0.11.14
   # remote source using git sha: DOES NOT WORK in tf 0.12.9 - 2nd module has no dotfile included
-  # source = "git@github.com:kipkoan/tf-0.12-dotfiles-bug.git//modules/tf_dotfiles?ref=92d2d80821a"
+  # source = "git@github.com:kipkoan/tf-0.12-dotfiles-bug.git//modules/tf_dotfiles?ref=a8bdfcc3a46"
 }
 
 module "tf_dotfiles_2" {
   # use the corresponding source line from module tf_dotfiles_1 above
 
   # source = "../../modules/tf_dotfiles"
-  # source = "git::../..//modules/tf_dotfiles?ref=92d2d80821a"
+  # source = "git::../..//modules/tf_dotfiles?ref=a8bdfcc3a46"
   source = "git@github.com:kipkoan/tf-0.12-dotfiles-bug.git//modules/tf_dotfiles"
-  # source = "git@github.com:kipkoan/tf-0.12-dotfiles-bug.git//modules/tf_dotfiles?ref=92d2d80821a"
+  # source = "git@github.com:kipkoan/tf-0.12-dotfiles-bug.git//modules/tf_dotfiles?ref=a8bdfcc3a46"
 }
